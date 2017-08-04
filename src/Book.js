aa7fb27bbdf1e7d67d079f46a9223fc009e5ac3b
Andrey Nikonov <a.einomi@gmail.com>
2017-08-04 23:49:13 +0300

    feat: Add book authors

--- src/Book.js
+++ src/Book.js
@@ -33,7 +33,7 @@ class Book extends React.Component {
                     </div>
                 </div>
                 <div className="book-title">{this.props.title}</div>
-                <div className="book-authors">{this.props.author}</div>
+                <div className="book-authors">{this.props.authors.join(', ')}</div>
             </div>
         );
     }
